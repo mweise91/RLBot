f7c8db477fa1172d09f9907f1dd123e2ce940a51
mweise91 <mweise@students.kennesaw.edu>
2019-01-18 09:43:54 -0500

    Update DataPacket.java

--- RLBotJava/src/main/java/rlbotexample/input/DataPacket.java
+++ RLBotJava/src/main/java/rlbotexample/input/DataPacket.java
@@ -5,13 +5,6 @@ import rlbot.flat.GameTickPacket;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * This class is here for your convenience, it is NOT part of the framework. You can change it as much
- * as you want, or delete it. The benefits of using this instead of rlbot.flat.GameTickPacket are:
- * 1. You end up with nice custom Vector3 objects that you can call methods on.
- * 2. If the framework changes its data format, you can just update the code here
- * and leave your bot logic alone.
- */
 public class DataPacket {
 
     /** Your own car, based on the playerIndex */
